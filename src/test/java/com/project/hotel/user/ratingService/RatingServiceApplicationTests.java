15a1da43a56ac7c1dedeb139f3511c85fa0ceaf9
Somenath95 <connectsomenath@gmail.com>
2024-05-15 11:14:57 +0530

    Initial Commit

---
+++ src/test/java/com/project/hotel/user/ratingService/RatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hotel.user.ratingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
